5a396c0de0360d0db2f22eca2efe45f6f81e9520
fifa f <piorus9196@gmail.com>
2023-02-19 20:01:01 +0100

    start working on RWD

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./style.css";
 import Swiper from "swiper/bundle";
-
 import "swiper/css/bundle";
 const listItems = document.querySelectorAll("#myList li");
+const numbers = document.querySelectorAll(".num");
 let activeItem = null;
 listItems.forEach((item, index) => {
   item.clicks = 0;
@@ -16,6 +16,30 @@ listItems.forEach((item, index) => {
     activeItem = item;
   });
 });
+
+////////////number countdown
+let interval = 5000;
+const countdown = (interval) => {
+  numbers.forEach((number) => {
+    let startValue = 0;
+    const endValue = +number.getAttribute("value");
+
+    let duration = Math.floor(interval / endValue);
+
+    const counter = setInterval(() => {
+      startValue++;
+      if (endValue > 2000) {
+        startValue += 4;
+      }
+      number.textContent = startValue;
+      if (startValue === endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  });
+};
+countdown(interval);
+/////
 
 const mySwiper = new Swiper(".mySwiper", {
   spaceBetween: 1,
